3c9ba8202250180a89868721e80e0f4b7e0a4d20
veerviren <virenvariya7@gmail.com>
2023-03-27 21:26:25 +0530

    code added

--- Hackerrank-SQL/Weather_Observation_Station_11.sql
+++
@@ -1,2 +0,0 @@
-SELECT DISTINCT CITY  FROM STATION WHERE left (CITY , 1) NOT IN ('a', 'e', 'i', 'o', 'u') 
-OR right (CITY , 1) NOT IN ('a', 'e', 'i', 'o', 'u')
---
+++ Hackerrank-SQL/Weather_Observation_Station_12.sql
@@ -0,0 +1,2 @@
+SELECT DISTINCT CITY FROM STATION WHERE left(CITY, 1) NOT IN('a', 'e', 'i', 'o', 'u')
+AND right(CITY, 1) NOT IN('a', 'e', 'i', 'o', 'u')

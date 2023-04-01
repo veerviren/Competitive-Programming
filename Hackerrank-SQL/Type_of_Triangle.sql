1aab7fd33806adf3e03f5e8cac254dd82369bd57
veerviren <virenvariya7@gmail.com>
2023-04-01 19:29:24 +0530

    Type of Triangle

---
+++ Hackerrank-SQL/Type_of_Triangle.sql
@@ -0,0 +1,1 @@
+select case when(A >= B + C) or (B >= A + C) or (C >= A + B) then 'Not A Triangle' when(A = B) and (B = C) and (C = A) then 'Equilateral' when(A<> B) and (B<> C) and (C<> A) then 'Scalene' else 'Isosceles' end as category from triangles;
